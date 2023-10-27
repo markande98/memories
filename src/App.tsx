01f8986cb2c4ca7af861a4534986fed313c3d4f8
markande98 <gauravbpit1998@gmail.com>
2023-10-27 09:18:42 +0530

    shadcn setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
-    <div>
-      <h1>Hello from react</h1>
+    <div className="text-2xl text-gray-500">
+      <h1 className="text-2xl text-blue-400">Hello from react</h1>
     </div>
   );
 };
